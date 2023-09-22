b3497418ef75f12b5cb24191dfa2c1f949dad4e6
Diego Iboy <diboy@bytesw.com>
2023-09-22 15:47:41 -0600

    arreglos para build

---
+++ src/blog/CreateBlog.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+const URI = 'http://23.21.20.142:8081/blogs/'
+
+
+const CompCreateBlog = () => {
+
+    const [title, settitle] = useState('')
+    const [content, setContent] = useState('')
+    const navigate = useNavigate()
+
+    //procedimiento para crear
+    const store = async (e) => {
+        e.preventDefault()
+        await axios.post(URI, { title: title, content, content })
+        navigate('/')
+    }
+
+    return (
+        <div>
+            <h3>Crear POST</h3>
+            <form onSubmit={store}>
+                <div className='mb-3'>
+                    <label className='form-label'>Titulo</label>
+                    <input
+                        value={title}
+                        onChange={(e) => settitle(e.target.value)}
+                        type='text'
+                        className='form-control'
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Contenido</label>
+                    <input
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        type='text'
+                        className='form-control'
+                    />
+                </div>
+                <button type='submit' className='btn btn-primary'><i className="fa-solid fa-plus fa-shake fa-lg"></i></button>
+            </form>
+        </div>
+    )
+}
+
+export default CompCreateBlog
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import logo from './logo.svg';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+
+
+import CompShowBlogs from './blog/ShowBlogs.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CompCreateBlog from './blog/CreateBlog';
+import CompEditBlog from './blog/EditBlog';
+
+
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<CompShowBlogs/>}/>
+          <Route path='/create' element={<CompCreateBlog/>}/>
+          <Route path='/edit/:id' element={<CompEditBlog/>}/>
+        </Routes>
+        </BrowserRouter>
   </React.StrictMode>
 );
 
